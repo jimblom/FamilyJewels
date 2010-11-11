f04bf2b6ffbd535e7ddaa2463ce2a7a985945a95
Jim Lindblom <jim@sparkfun.com>
2010-11-11 13:27:28 -0700

    Working game code, needs to be ported to a hardware platform

--- source/main.c
+++ source/main.c
@@ -8,6 +8,7 @@
 //======================//
 #include <avr/io.h>
 #include <stdlib.h>
+#include <avr/interrupt.h>
 #include "spi.h"
 #include "uart.h"
 #include "bejeweled.h"
@@ -28,6 +29,8 @@
 //  Golbal Definitions	//
 //======================//
 #define BOARD_SIZE 8
+#define HS_ADDRESS_L 1
+#define HS_ADDRESS_H 2
 
 //======================//
 // Function Definitions	//
@@ -35,10 +38,17 @@
 void ioinit(void);
 void delay_ms(uint16_t x);
 void delay_us(uint16_t x);
+void writeEEPROM(char toWrite, uint8_t addr);
+char readEEPROM(char addr);
 
 void generateSeed(void);
 void initBoard(void);
 void printBoard(void);
+int validBoard(void);
+void removeGems(void);
+void addGems(void);
+int noMovesLeft(void);
+uint16_t getHighScore(void);
 
 //======================//
 //   Global Variables	//
@@ -53,6 +63,15 @@ char currentBoard[BOARD_SIZE][BOARD_SIZE] = {
 {0,0,0,0,0,0,0,0},
 {0,0,0,0,0,0,0,0}};
 char newBoard[BOARD_SIZE][BOARD_SIZE];
+int score = 0;
+int timeLimit = 60;
+int gameTimer = 0;
+int highScore;
+
+ISR(TIMER1_COMPA_vect)
+{
+	gameTimer++;
+}
 
 //======================//
 //	       Main			//
@@ -61,32 +80,26 @@ int main(void)
 {
 	uint8_t userInput[4];
 	uint8_t row1, row2, column1, column2;
-	uint8_t count = 0;
-	char temp;
 	
-	ioinit();		// Initialize I/O
-	initBoard();	// Create a random 8x8 board
+	ioinit();
 	
-	// Make new board equal to current board
-	for (uint8_t i=0; i<BOARD_SIZE; i++)
-	{
-		for (uint8_t j=0; j<BOARD_SIZE; j++)
-		{
-			newBoard[i][j] = currentBoard[i][j];
-		}
-	}
+	/* Reset high scores
+	writeEEPROM(0, HS_ADDRESS_L);
+	writeEEPROM(0, HS_ADDRESS_H);
+	*/
+	highScore = getHighScore();
+	printf("Can you beat %d?\n", highScore);
 	
-	// loop 0 waiting for the game to start - does not start till correct move is made
+	initBoard();
+	printBoard();
 	while(1)
 	{
-		printBoard();
-		// wait for user input store as button 0
-		// wait for user input store as button 1
-		printf("Enter row column row column...\n");
+		//--- get user input---
+		printf("Enter column row column row...\n");
 		for (unsigned int i = 0; i<4; i++)
 		{
 			userInput[i] = uart_getchar() - 0x30;
-			printf("%d", userInput[i]);
+			printf("%d ", userInput[i]);
 			// 0 - row 1
 			// 1 - column 1
 			// 2 - row 2
@@ -95,79 +108,88 @@ int main(void)
 		row1 = userInput[0];
 		column1 = userInput[1];
 		row2 = userInput[2];
-		column2 = userInput[3];
+		column2 = userInput[3];	
+		printf("\n");
 		
-		printf("\n\n");
-		
-		// if buttons legal (next to, or on top/bottom of eachother)
-		// First check horizontal move
-		if((row1 == row2)&&((column1==column2+1)||(column1==column2-1)))
+		// --- check valid move ---
+		if (((column1 == column2 && (row1 == row2 + 1)) ||
+			(column1 == column2 && (row1 == row2 - 1)) ||
+			(row1 == row2 && (column1 == column2 + 1)) ||
+			(row1 == row2 && (column1 == column2 - 1))))
 		{
-			newBoard[row1][column1] = currentBoard[row1][column2];
-			newBoard[row1][column2] = currentBoard[row1][column1];
+			// --- swap inputs ---
+			currentBoard[column1][row1] = currentBoard[column1][row1] ^ currentBoard[column2][row2];
+			currentBoard[column2][row2] = currentBoard[column2][row2] ^ currentBoard[column1][row1];
+			currentBoard[column1][row1] = currentBoard[column1][row1] ^ currentBoard[column2][row2];
 			
-			printf("Horizontal move\n");
-			printf("Swapping %d with %d\n", currentBoard[row1][column1], currentBoard[row2][column2]);
-			// Check if gems match
-			// Check to left
-			count = 0;
-			while((column1-count>=0)&&(currentBoard[row1][column2]==currentBoard[row1][column1-count-1]))
-			{
-				count++;
+			if (!validBoard())
+			{	// If there's no valid moves revert to old board
+				currentBoard[column1][row1] = currentBoard[column1][row1] ^ currentBoard[column2][row2];
+				currentBoard[column2][row2] = currentBoard[column2][row2] ^ currentBoard[column1][row1];
+				currentBoard[column1][row1] = currentBoard[column1][row1] ^ currentBoard[column2][row2];
+				printf("Nothing to eliminate\n");
 			}
-			printf("left count==%d\n", count);
-			
-			// Delete matching gems
-			// move the above gems down
-			// generate new gems
-			// increment score
-			// exit loop 0
+			else
+			{
+				while (validBoard())
+				{
+					removeGems();
+					addGems();
+					printBoard();
+					printf("Score: %d\n", score);
+					printf("Timer: %d/%d\n", gameTimer, timeLimit);
+				}/*
+				if(validBoard())
+				{
+					while(validBoard())
+					{
+						removeGems();
+						addGems();
+						printBoard();
+					}
+				}*/
+				if(noMovesLeft())
+					break;
+			}
 		}
-		// Then check vertical move
-		else if ((column1 == column2)&&((row1==row2+1)||(row1==row2-1)))
-		{
-			printf("Vertical move\n");
-			printf("Swapping %d with %d\n", currentBoard[userInput[0]][userInput[1]], currentBoard[userInput[2]][userInput[3]]);
-			uint8_t count = 0;
-			while(((userInput[1]-count)>=0)||((currentBoard[userInput[0]][userInput[1]]==currentBoard[userInput[0]][userInput[1-count]])))
-				count++;
-			printf("count == %d\n", count);
-		}
-		// if move is incorrect
 		else
 		{
-			// shortly blink incorrect gems
-			// go to top of loop 0
-			printf("Illegal move, please try again\n");
+			printf("Invalid Move\n");
+		}	
+		
+		if (gameTimer >= timeLimit)
+		{
+			printf("Out of time!!!\n");
+			break;
 		}
-				
+		else if (noMovesLeft())
+		{
+			printf("No moves remain\n");
+			break;
+		}
+		// !!!To DO!!!
+		// else increase speed of timer go back to top of while
 	}
-	// end loop 0
-	
-	// start timer constantly (interrupt) decrement timer, check for end
-	
-	// loop 1
-		// wait for user input store as button 0
-		// wait for user input store as button 1
-		// if buttons legal (next to, or on top/bottom of eachother)
-			// if move is correct
-				// Delete matching gems
-				// move the above gems down
-				// generate new gems
-				// increment score
-				// exit loop 0
-			// if move is incorrect
-				// shortly blink incorrect gems
-	// end loop 1
-	
-	while(1)
+	printf("Game Over!\n");
+	printf("Final Score: %d\n", score);
+	if (score > highScore)
 	{
+		printf("That's a new high score!\n");
+		writeEEPROM((score & 0xFF00)>>8, HS_ADDRESS_H);
+		writeEEPROM((score & 0xFF), HS_ADDRESS_L);
 	}
 }
 
 void ioinit (void)
 {
 	int myUBRR = 103;	// Results in 9600bps@8MHz or 19200bps@16MHz
+		
+	// Set 16-bit Timer 1 to 1Hz
+	TCCR1A = 0x00;	// no output, CTC operation
+	TCCR1B = (_BV(WGM12) | _BV(CS12));	// CTC operation, 256 prescaler
+	OCR1A = F_CPU/256;
+	TIMSK1 = _BV(OCIE1A);	// Overflow interrupt enable
+	sei();
 	
     //1 = output, 0 = input
     DDRB = 0b11101101; //MISO input, PB1 input
@@ -189,19 +211,12 @@ void initBoard()
 		for (uint8_t j=0; j<BOARD_SIZE; j++)
 		{
 			currentBoard[i][j] = rand()%7;
-		}
-	}
-	
-	// Make sure that there are no repetitions of 3
-	for (uint8_t i=0; i<BOARD_SIZE; i++)
-	{
-		for (uint8_t j=0; j<BOARD_SIZE; j++)
-		{
-			while ((currentBoard[i][j]==currentBoard[i-1][j])&&(currentBoard[i][j]==currentBoard[i-2][j])&&(i>=2))
+			
+			while (currentBoard[i][j] == currentBoard[i-1][j] && currentBoard[i][j] == currentBoard[i-2][j] && i >= 2)
 			{
 				currentBoard[i][j] = rand()%7;
 			}
-			while ((currentBoard[i][j]==currentBoard[i][j-1])&&(currentBoard[i][j]==currentBoard[i][j-2])&&(j>=2))
+			while (currentBoard[i][j] == currentBoard[i][j-1] && currentBoard[i][j] == currentBoard[i][j-2] && j >= 2)
 			{
 				currentBoard[i][j] = rand()%7;
 			}
@@ -231,14 +246,182 @@ void printBoard(void)
 {
 	// Print board out to terminal
 	printf("\n");
+	printf("   0 1 2 3 4 5 6 7\n");
+	printf("------------------\n");
 	for (uint8_t i=0; i<BOARD_SIZE; i++)
 	{
+		printf("%d: ", i);
 		for (uint8_t j=0; j<BOARD_SIZE; j++)
 		{
-			printf("%d", currentBoard[i][j]);
+			printf("%d ", currentBoard[i][j]);
 		}
 		printf("\n");
 	}
+}
+
+int validBoard(void)
+{
+	int i, j, valid = 0;
+
+	for (j=0; j<BOARD_SIZE; j++)
+	{
+		for (i=0; i<BOARD_SIZE; i++)
+		{
+			if(currentBoard[i][j]==currentBoard[i+1][j] && 
+				currentBoard[i][j]==currentBoard[i+2][j] && 
+				i < BOARD_SIZE-2 && 
+				currentBoard[i][j] != 7)
+			{
+				valid = 1;
+			}
+			if(currentBoard[i][j]==currentBoard[i][j+1] && 
+				currentBoard[i][j]==currentBoard[i][j+2] && 
+				j < BOARD_SIZE-2 && 
+				currentBoard[i][j] != 7)
+			{
+				valid = 1;
+			}
+		}
+	}
+	return valid;
+}
+
+void removeGems(void)
+{
+	int i, j;
+	char copyBoard[BOARD_SIZE][BOARD_SIZE];
+	
+	for (j=0; j<BOARD_SIZE; j++)
+	{
+		for (i=0; i<BOARD_SIZE; i++)
+		{
+			copyBoard[i][j] = 0;
+		}
+	}
+	
+	for(j=0; j<BOARD_SIZE; j++)
+	{
+		for(i=0; i<BOARD_SIZE; i++)
+		{
+			if(currentBoard[i][j] == currentBoard[i+1][j] && currentBoard[i][j] == currentBoard[i+2][j] && i < BOARD_SIZE-2)
+			// Test for 3+ in vertical line
+			{
+				while (currentBoard[i][j] == currentBoard[i+1][j] && currentBoard[i][j] != 7)
+				// Place 1's in spots where there are matches
+				{
+					copyBoard[i][j] = 1;
+					i++;
+				}
+				copyBoard[i][j] = 1;
+			}
+		}
+	}
+	for(j=0; j<BOARD_SIZE; j++)
+	{
+		for(i=0; i<BOARD_SIZE; i++)
+		{
+			if(currentBoard[i][j] == currentBoard[i][j+1] && currentBoard[i][j] == currentBoard[i][j+2] && j < BOARD_SIZE-2)
+			// New check for 3+ in a horizontal line
+			{
+				while (currentBoard[i][j] == currentBoard[i][j+1] && currentBoard[i][j] != 7)
+				// Place 1's in spots where there are matches
+				{
+					copyBoard[i][j] = 1;
+					j++;
+				}
+				copyBoard[i][j] = 1;
+			}
+		}
+	}
+
+	for (j=0; j<BOARD_SIZE; j++)
+	{
+		for (i=0; i<BOARD_SIZE; i++)
+		{
+			if (copyBoard[i][j] == 1)
+			{
+				currentBoard[i][j] = 7; // Every correct gem temporarily receives a 7
+				score++;
+				timeLimit+=2;
+			}
+
+		}
+	}
+}
+
+void addGems(void)
+{
+	int i, j, l;
+
+	for(i=0; i<BOARD_SIZE; i++)
+	{
+		for(j=0; j<BOARD_SIZE; j++)
+		{
+			if(currentBoard[i][j] == 7 && j == 0)
+			{
+				currentBoard[i][j] = rand()%7;
+				continue;
+			}
+			if(currentBoard[i][j] == 7 && j > 0)
+			{
+				for(l=j; l>0; l--)
+				{
+					currentBoard[i][l] = currentBoard[i][l-1];
+				}
+
+				currentBoard[i][0] = rand()%7;
+
+				continue;
+			}
+		}
+	}
+
+}
+
+int noMovesLeft(void)
+{
+	int moves, i, j;
+
+	for(i=0; i<BOARD_SIZE; i++)
+	{
+		for(j=0; j<BOARD_SIZE; j++)
+		{
+			if(currentBoard[i][j] == currentBoard[i][j+1] && (currentBoard[i][j] == currentBoard[i][j+3]
+			|| currentBoard[i][j] == currentBoard[i-1][j+2] || currentBoard[i][j] == currentBoard[i+1][j+2]
+			|| currentBoard[i][j] == currentBoard[i][j-2] || currentBoard[i][j] == currentBoard[i-1][j-1]
+			|| currentBoard[i][j] == currentBoard[i+1][j-1]))
+			// Checking horizontal
+			{
+				moves = 1;
+			}
+			else
+			{
+				if(currentBoard[i][j] == currentBoard[i+1][j] && (currentBoard[i][j] == currentBoard[i+3][j]
+				|| currentBoard[i][j] == currentBoard[i+2][j-1] || currentBoard[i][j] == currentBoard[i+2][j+1]
+				|| currentBoard[i][j] == currentBoard[i-2][j] || currentBoard[i][j] == currentBoard[i-1][j-1]
+				|| currentBoard[i][j] == currentBoard[i-1][j+1]))
+				// Checking vertical
+				{
+					moves = 1;
+				}
+				else
+				{
+					moves = 0;
+				}
+			}
+		}
+	}
+	return moves;
+}
+
+uint16_t getHighScore(void)
+{
+	uint8_t hsLow, hsHigh;
+	
+	hsLow = readEEPROM(HS_ADDRESS_L);
+	hsHigh = readEEPROM(HS_ADDRESS_H);
+	
+	return (hsHigh << 8) | hsLow;
 }
 
 //General short delays
@@ -265,3 +448,32 @@ void delay_us(uint16_t x)
     while( (TIFR2 & (1<<TOV2)) == 0);
 }
 
+// Write toWrite to addr in EEPROM
+void writeEEPROM(char toWrite, uint8_t addr)
+{
+	// Write toWrite value to EEPROM address addr
+	/* Wait for completion of previous write */
+	while(EECR & (1<<EEPE))
+		;
+	/* Set up address and Data Registers */
+	EEAR = addr;
+	EEDR = toWrite;	// Write data1 into EEPROM
+	/* Write logical one to EEMPE */
+	EECR |= (1<<EEMPE);
+	/* Start eeprom write by setting EEPE */
+	EECR |= (1<<EEPE);
+}
+
+// Read EEPROM address addr and return value in EEDR
+char readEEPROM(char addr)
+{
+	/* Wait for completion of previous write */
+	while(EECR & (1<<EEPE))
+		;
+	/* Set up address register */
+	EEAR = addr;
+	/* Start eeprom read by writing EERE */
+	EECR |= (1<<EERE);
+
+	return EEDR;
+}
